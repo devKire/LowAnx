2b60623b1bab67e494dca71a5d914ca5f6b9f528
devKire <erikdossantos2006@outlook.com>
2024-05-04 14:06:37 -0300

    Update 04/05/2024

--- LowAnx/src/pages/BookPage.tsx
+++ LowAnx/src/pages/BookPage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Tabs from "../components/Tabs";
 import TabPane from "../components/TabPane";
 import Footer from "../components/footer";
--- LowAnx/src/pages/SearchPage.tsx
+++ LowAnx/src/pages/SearchPage.tsx
@@ -1,5 +1,3 @@
-import { Link } from "react-router-dom"
-import { routes } from "../router/routes"
 import Footer from "../components/footer"
 import Divider from "../components/divider"
 import Header from "../components/header"
